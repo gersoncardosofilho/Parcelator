983816fe92c49c5512e070798fe67c38c0691f5a
GERSON CARDOSO FILHO <gersoncfilho@gmail.com>
2017-11-05 22:04:34 -0200

    implementa sharing

--- app/src/main/java/com/developer/gerson/parcelator/MainActivity.java
+++ app/src/main/java/com/developer/gerson/parcelator/MainActivity.java
@@ -2,6 +2,7 @@ package com.developer.gerson.parcelator;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.icu.text.DecimalFormatSymbols;
 import android.os.Debug;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,9 @@ import android.support.v4.math.MathUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
@@ -17,9 +21,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.developer.gerson.parcelator.Activity.MainActivity2;
+import com.developer.gerson.parcelator.Model.Calculos;
 import com.xw.repo.BubbleSeekBar;
 
 import java.io.Console;
+import java.io.Serializable;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -29,7 +36,7 @@ import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity implements ICalculos {
 
-    private Button calcularButton;
+    private Button calcularButton, limparButton;
     private EditText
             etValorPrincipal,
             etTaxaDebito,
@@ -44,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements ICalculos {
             tvValorTarifaDebito,
             tvValorTarifaCredito,
             tvValorTarifaParcelado,
-            tvValorParcela;
+            tvValorParcela, tvValorPrincipal;
 
     private BubbleSeekBar bubbleSeekBarParcelas;
 
@@ -89,7 +96,48 @@ public class MainActivity extends AppCompatActivity implements ICalculos {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =  getMenuInflater();
+        inflater.inflate(R.menu.menu_parcelator, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.share:
+                compartilhar();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void compartilhar(){
+        if (tvValorTotalDebito.getText().toString() == "")
+            return;
+
+        Double _valorPrincipal = GetValorPrincipal();
+
+        Calculos calculos = new Calculos();
+        calculos.setValorPrincipal(nf.format(_valorPrincipal));
+        calculos.setTarifaDebito(tvValorTarifaDebito.getText().toString());
+        calculos.setTarifaCredito(tvValorTarifaCredito.getText().toString());
+        calculos.setTarifaParcelado(tvValorTarifaParcelado.getText().toString());
+        calculos.setValorDebito(tvValorTotalDebito.getText().toString());
+        calculos.setValorCredito(tvValorTotalCredito.getText().toString());
+        calculos.setValorParcelado(tvValorTotalParcelado.getText().toString());
+        calculos.setQuantidadeDeParcelas(quantidadeParcelas);
+        calculos.setValorDaParcela(tvValorParcela.getText().toString());
+
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("calculos", (Serializable) calculos);
+        startActivity(intent);
+    }
+
     private void initializeComponents() {
+
         etValorPrincipal = findViewById(R.id.editTextValorPrincipal);
         etTaxaDebito = findViewById(R.id.editTextTaxaDebito);
         tvValorTarifaDebito = findViewById(R.id.textViewValorTarifaDebito);
@@ -103,9 +151,24 @@ public class MainActivity extends AppCompatActivity implements ICalculos {
         calcularButton = findViewById(R.id.button);
         bubbleSeekBarParcelas = findViewById(R.id.bubleSeekParcelas);
         tvValorParcela = findViewById(R.id.textViewValorParcela);
+        limparButton = findViewById(R.id.buttonLimpar);
     }
 
+    private void limparCampos(){
+        etValorPrincipal.setText("");
+        tvValorTotalCredito.setText("");
+        tvValorTotalDebito.setText("");
+        tvValorTotalParcelado.setText("");
+        tvValorTarifaDebito.setText("");
+        tvValorTarifaCredito.setText("");
+        tvValorTarifaParcelado.setText("");
+    }
+
+
+
     private void AtualizaValores(int quantidadeParcelas) {
+
+
         //Debito
 
         Double taxaDebito = Double.parseDouble(etTaxaDebito.getText().toString());
@@ -113,6 +176,7 @@ public class MainActivity extends AppCompatActivity implements ICalculos {
         Double formulaDebito = GetFormula(taxaDebito);
         Double valorTotal = valorPrincipal / formulaDebito;
         Double valorTarifaDebito = GetValorTaxa(taxaDebito, valorPrincipal);
+
 
         Log.i("Taxa: ===>", taxaDebito.toString());
         Log.i("Valor principal ===>", GetValorPrincipal().toString());
@@ -136,13 +200,18 @@ public class MainActivity extends AppCompatActivity implements ICalculos {
 //        //Parcelado
         Double taxaParcelado = Double.parseDouble(etTaxaParcelado.getText().toString());
         Double formulaParcelado = GetFormula(taxaParcelado);
+
         Double valorTotalParcelado = valorPrincipal / formulaParcelado;
+
+
         Double valorTarifaParcelado = GetValorTaxa(taxaParcelado, valorPrincipal);
         Double valorParcela = valorTotalParcelado / quantidadeParcelas;
 
         tvValorTarifaParcelado.setText(nf.format(valorTarifaParcelado));
         tvValorTotalParcelado.setText(nf.format(valorTotalParcelado));
         tvValorParcela.setText(nf.format(valorParcela));
+
+
     }
 
     private void doCalculation(final int parcelas) {
@@ -166,7 +235,16 @@ public class MainActivity extends AppCompatActivity implements ICalculos {
             }
         });
 
+        limparButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limparCampos();
+            }
+        });
+
     }
+
+
 
     @Override
     public Double GetFormula(Double taxa) {
